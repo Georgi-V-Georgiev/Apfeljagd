9e10a5e27361fe47f28c152b5c5d114b32dd021f
Yoana Blagova <blagovayoana@gmail.com>
2023-07-02 16:12:42 +0200

    created main menu

---
+++ Assets/_SCRIPTS/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Story()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("You have quit the game!");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadSceneAsync(5);
+
+    }
+}
