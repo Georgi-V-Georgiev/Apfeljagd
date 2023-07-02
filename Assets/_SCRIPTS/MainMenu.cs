460cd109684524b84ec5ed323f12c5f892c53b32
Maria Todorova <m.todorova@blubito.com>
2023-07-02 18:21:24 +0300

    rearranged the scenes with the main menu

--- Assets/_SCRIPTS/MainMenu.cs
+++ Assets/_SCRIPTS/MainMenu.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void StartGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
 
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
     public void Story()
     {
         SceneManager.LoadSceneAsync(4);
